48d34f8998315e679690c47db9cefed9bbfdfe29
KavyaSek <ksekar@chicagobooth.edu>
2024-11-06 02:24:25 +0000

    dirbiodone

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "disp_directors" })
   get("/actors", { :controller => "actors", :action => "disp_actors" })
   get("/movies", { :controller => "movies", :action => "disp_movies" })
+  get("/directors/:dir_id", { :controller => "directors_bio", :action => "disp_directors_bio" })
 
 end
